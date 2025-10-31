0f7eca09a5f0b9c82d913ac18975446ec9d32a72
Jenny <jenny.christopher@multicorewareinc.com>
2025-10-31 16:40:15 +0530

    Modify yaml files and path

--- op_tests/tests/test_addcmul.py
+++ op_tests/tests/test_addcmul.py
@@ -1,8 +1,9 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../math_ops"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../addcmul_math_op/math_ops"))
 
 from addcmul import addcmul
+
 
 def test_addcmul_basic():
     assert addcmul(1,2,3) == 7
